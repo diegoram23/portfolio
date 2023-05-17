9fb2925205d997bd7bd15a6503c9d6d473841b2a
diegoram23 <diego.pta.oc@gmail.com>
2023-05-16 22:07:07 -0700

    installed Email JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const formName = document.getElementById('name')
+const formBtn = document.getElementById('form-btn')
+
+formBtn.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('value',nameVal);
+})
+
+
