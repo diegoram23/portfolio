1f43c2c810dff75f387d4d2e6634eb819197d564
diegoram23 <diego.pta.oc@gmail.com>
2023-05-17 12:28:48 -0700

    EmailJS set up and working

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const formName = document.getElementById('name')
 const formBtn = document.getElementById('form-btn')
+const formEmail = document.getElementById('email')
+const formMessage = document.getElementById('message')
 
-formBtn.addEventListener('submit', (e) => {
+
+formBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log('value',nameVal);
+    sendMail()
 })
+
+function sendMail() {
+    let params = {
+        name: formName.value,
+        email: formEmail.value,
+        message: formMessage.value
+    }
+
+    const serviceId = 'service_tknhlvn'
+    const templateId = 'template_tqqozvc'
+
+    emailjs.send(serviceId, templateId, params)
+        .then(
+            res => {
+                formName.value = ''
+                formEmail.value = ''
+                formMessage.value = ''
+                console.log(res);
+                alert('message sent succesfully')
+            })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
 
 
