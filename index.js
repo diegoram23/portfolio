c62c9f33426ca9092655e7ef0f54e63df47a9697
diegoram23 <diego.pta.oc@gmail.com>
2023-05-17 19:44:06 -0700

    Modal pop up added after sending message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const formName = document.getElementById('name')
 const formBtn = document.getElementById('form-btn')
 const formEmail = document.getElementById('email')
 const formMessage = document.getElementById('message')
+const modal = document.getElementById('modal')
+const modalBtn = document.getElementById('modal-btn')
 
 
 formBtn.addEventListener('click', (e) => {
@@ -9,7 +11,10 @@ formBtn.addEventListener('click', (e) => {
     sendMail()
 })
 
-function sendMail() {
+const sendMail = () => {
+    formBtn.disabled = true
+        formBtn.textContent = 'Sending'
+
     let params = {
         name: formName.value,
         email: formEmail.value,
@@ -25,13 +30,20 @@ function sendMail() {
                 formName.value = ''
                 formEmail.value = ''
                 formMessage.value = ''
-                console.log(res);
-                alert('message sent succesfully')
+                messageSentModal()
             })
         .catch(error => {
             console.log(error)
         })
+        formBtn.textContent = 'Send Message'
+    formBtn.disabled = false
 }
 
+modalBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
 
+const messageSentModal = () => {
+    modal.style.display = 'inline'
+}
 
