8b4826aa118f8c36e5fcb07e436500d35c3c3ad4
diegoram23 <diego.pta.oc@gmail.com>
2023-05-27 09:44:06 -0700

    Media queries styling for screen width over 850px

--- projectsData.js
+++ projectsData.js
@@ -11,17 +11,6 @@ export const data = [
         id: 0
     },
 
-    {
-        title: "Market Scoop",
-        coverPhoto: "./images/medium.png",
-        projectText: `Get the latest news on the stock market, find out more about an individual ticker,
-        and create a watchlist. Built with <span class="blue">React</span> and a <span class="orange">local 
-        storage database.</span></p>`,
-        liveLink: "https://diegoram23.github.io/stock-market-watchlist/",
-        codeLink: "https://github.com/diegoram23/stock-market-watchlist",
-        altTag: "homepage of market scoop website",
-        id: 1
-    },
 
     {
         title: "CPR-Quiz",
@@ -31,20 +20,30 @@ export const data = [
         liveLink: "https://diegoram23.github.io/cpr-quiz/",
         codeLink: "https://github.com/diegoram23/cpr-quiz",
         altTag: "homepage of cpr-quiz website",
-        id: 2
+        id: 1
     },
 
+    {
+        title: "Market Scoop",
+        coverPhoto: "./images/medium.png",
+        projectText: `Get the latest news on the stock market, find out more about an individual ticker,
+        and create a watchlist. Built with <span class="blue">React</span> and a <span class="orange">local 
+        storage database.</span></p>`,
+        liveLink: "https://diegoram23.github.io/stock-market-watchlist/",
+        codeLink: "https://github.com/diegoram23/stock-market-watchlist",
+        altTag: "homepage of market scoop website",
+        id: 2
+    },
 
     {
         title: "Quick Rehab Notes",
         coverPhoto: "./images/doc-extension.png",
         projectText: `Designed for physical therapists to speed up your documentation with this simple
-        effective app. Ready to generate texts with a simple click. Built with html and <span class="orange">Javascript.</span></p>`,
+        effective app. Generate texts with a simple click. Built with html and <span class="orange">Javascript.</span></p>`,
         liveLink: "https://diegoram23.github.io/documentation-extension/",
         codeLink: "https://github.com/diegoram23/documentation-extension",
         altTag: "homepage of Quick Rehab Notes website",
         id: 3
     },
-
 
 ]
