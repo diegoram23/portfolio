b06f9023bd39bd18f8b34e08435006ab797f1e29
diegoram23 <diego.pta.oc@gmail.com>
2023-05-30 07:51:21 -0700

    Updated project text

--- projectsData.js
+++ projectsData.js
@@ -2,9 +2,8 @@ export const data = [
     {
         title: "Rehab Docs",
         coverPhoto: "./images/large1.png",
-        projectText: `A mock documentation service for physical therapy treatments. Inspired by my own
-        work experience. Built with a <span class="blue">React</span> frontend and <span
-            class="orange">Firebase</span> backend.</p>`,
+        projectText: `Based on the Net Health software for physical therapists offering analytics, documentation, and electronic medical records. 
+        Built with a <span class="blue">React</span> frontend and <span class="orange">Firebase</span> backend.</p>`,
         liveLink: "https://diegoram23.github.io/rehab-docs/",
         codeLink: "https://github.com/diegoram23/rehab-docs",
         altTag: "homepage of rehab-docs website",
@@ -26,9 +25,8 @@ export const data = [
     {
         title: "Market Scoop",
         coverPhoto: "./images/medium.png",
-        projectText: `Get the latest news on the stock market, find out more about an individual ticker,
-        and create a watchlist. Built with <span class="blue">React</span> and a <span class="orange">local 
-        storage database.</span></p>`,
+        projectText: `API calls fetch the latest market news, search any stock on the NYSE to get detailed history, current prices and create a watchlist.
+        Built with <span class="blue">React</span> and a <span class="orange">local storage database.</span></p>`,
         liveLink: "https://diegoram23.github.io/stock-market-watchlist/",
         codeLink: "https://github.com/diegoram23/stock-market-watchlist",
         altTag: "homepage of market scoop website",
@@ -38,8 +36,8 @@ export const data = [
     {
         title: "Quick Rehab Notes",
         coverPhoto: "./images/doc-extension.png",
-        projectText: `Designed for physical therapists to speed up your documentation with this simple
-        effective app. Generate texts with a simple click. Built with html and <span class="orange">Javascript.</span></p>`,
+        projectText: `Designed for physical therapists to speed up documentation time by generating custom statements based on session interventions.
+        Built with html and <span class="orange">Javascript.</span></p>`,
         liveLink: "https://diegoram23.github.io/documentation-extension/",
         codeLink: "https://github.com/diegoram23/documentation-extension",
         altTag: "homepage of Quick Rehab Notes website",
